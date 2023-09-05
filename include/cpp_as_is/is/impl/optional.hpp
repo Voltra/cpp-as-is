ff7c6c65cfdae695f66b3f1432d0d553e0367993
Voltra <ludwig.guerin.98@gmx.fr>
2023-09-05 16:35:07 +0200

    Add specialization of IS for std::optional

---
+++ include/cpp_as_is/is/impl/optional.hpp
@@ -0,0 +1,23 @@
+#ifndef CPP_AS_IS_OPTIONAL_HPP
+#define CPP_AS_IS_OPTIONAL_HPP
+
+#include "../traits.hpp"
+#include <optional>
+#include <type_traits>
+
+namespace cpp_as_is {
+template<class T> struct is_traits<std::optional<T>>
+{
+  template<class U> constexpr static bool matches_type(const std::optional<T> &opt) noexcept
+  {
+    return std::is_same_v<T, U> && opt.has_value();
+  }
+
+  template<> constexpr static bool matches_type<std::nullopt_t>(const std::optional<T> &opt) noexcept
+  {
+    return !opt.has_value();
+  }
+};
+}// namespace cpp_as_is
+
+#endif// CPP_AS_IS_OPTIONAL_HPP
