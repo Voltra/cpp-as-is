d43e7dbd11920e62fd3e2c3ea92d9c712186f6e5
Voltra <ludwig.guerin.98@gmx.fr>
2023-09-10 11:36:12 +0200

    Refactor implementation of IS to be correct C++ and more easily extensible

--- include/cpp_as_is/cpp_as_is.hpp
+++ include/cpp_as_is/cpp_as_is.hpp
@@ -1,6 +1,24 @@
 #ifndef CPP_AS_IS_CPP_AS_IS_HPP
 #define CPP_AS_IS_CPP_AS_IS_HPP
 
+/**
+ * @namespace cpp_as_is
+ * Root namespace of the library
+ */
+namespace cpp_as_is {
+/**
+ * @namespace cpp_as_is::ext
+ * Sub-namespace for extension points
+ */
+namespace ext {}
+
+/**
+ * @namespace cpp_as_is::details
+ * Sub-namespace for implementation details and utility functions
+ */
+namespace details {}
+}
+
 #include "./is/is.hpp"
 #include "./is/traits.hpp"
 
